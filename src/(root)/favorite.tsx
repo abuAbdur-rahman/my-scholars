6d4dd0c621a54ebb374500160ba0f8f2e12e61c9
Abu Abdirrahman <abdulazeezadekiilekun@gmail.com>
2024-11-30 13:06:12 +0100

    Library section

--- src/(root)/favorite.tsx
+++ src/(root)/favorite.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import LectureCard from '@/components/LectureCatd';
+
+let lecturesData: Lecture[]
 
 const Favorite = () => {
   return (
-    <div>favorite</div>
-  )
+    <div className='p-6'>
+      <h2 className='text-3xl font-semibold mb-4'>Your Favorite Lectures</h2>
+      <div className='space-y-4'>
+        {lecturesData.map((lecture) => (
+          <LectureCard key={lecture.id} lecture={lecture} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Favorite
